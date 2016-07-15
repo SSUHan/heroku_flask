d9bec40382690bcc24de58f2bd6bb116e09ee7c7
이준수 <ljs93kr@gmail.com>
2016-07-15 22:22:37 +0900

    assets add

--- app/routes/controller.py
+++ app/routes/controller.py
@@ -1,9 +1,9 @@
 from app.blueprint import basic
-from flask import jsonify
+from flask import jsonify, render_template
 
 @basic.route('/')
 def index():
-	return "hello ljs93kr!!"
+	return render_template('index.html')
 
 @basic.route('/data')
 def data():
