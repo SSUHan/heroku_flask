2f71618ad5a7633a4207a0df1b35802a582c0ec2
이준수 <ljs93kr@gmail.com>
2016-07-21 08:59:23 +0900

    jawsDB install

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/mytest?charset=utf8'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/mytest?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cyavulucaoc6n4mo:zsdd5b5zm0mcd2j4@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d6wpcbemofr48xfa'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
