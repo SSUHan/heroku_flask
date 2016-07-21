f124e4abe520527534a8dc98afa1b5698e7c729d
이준수 <ljs93kr@gmail.com>
2016-07-21 10:03:03 +0900

    show herrku

--- app/routes/controller.py
+++ app/routes/controller.py
@@ -17,18 +17,18 @@ def data():
 	data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
 	return jsonify(data)
 
-@app.route('/su_point/db_insert', methods=["GET"])
-def db_insert():
-	user_id = request.args.get('user_id')
-	user_name = request.args.get('user_name')
-	point = request.args.get('point')
-
-	person = User(user_id, user_name, point)
-	db.session.add(person)
-	db.session.commit()
-
-	output = User.query.all()
-	string = ""
-	for item in output:
-		string = string + item.user_id + " " + item.user_name + "<br>"
-	return string
+# @app.route('/su_point/db_insert', methods=["GET"])
+# def db_insert():
+# 	user_id = request.args.get('user_id')
+# 	user_name = request.args.get('user_name')
+# 	point = request.args.get('point')
+#
+# 	person = User(user_id, user_name, point)
+# 	db.session.add(person)
+# 	db.session.commit()
+#
+# 	output = User.query.all()
+# 	string = ""
+# 	for item in output:
+# 		string = string + item.user_id + " " + item.user_name + "<br>"
+# 	return string
